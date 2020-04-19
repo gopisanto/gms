5b39db63fbb20620fb489900b7bed31ec99a36c8
Santosh Kumar Gopisetty <skg@marketlogicsoftware.com>
2020-04-19 11:00:47 +0200

    style change for cart

--- src/components/GroceryListItem.style.js
+++ src/components/GroceryListItem.style.js
@@ -53,7 +53,7 @@ export default (theme => ({
     width: '100%',
     height: '100%',
     top: '0',
-    left: '0',
+    left: '12%',
     right: '0',
     bottom: '0',
     zIndex: '2',
@@ -61,7 +61,7 @@ export default (theme => ({
     color: 'red',
     opacity: '0.6',
     fontWeight: 'bold',
-    transform: 'rotate(-45deg)'
+    // transform: 'rotate(-45deg)'
   },
   outOfStockPrice: {
     backgroundColor: 'black',
