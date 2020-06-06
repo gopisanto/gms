6103d908cdc63df2f621bc94f097df5590390fa3
Santosh Kumar Gopisetty <skg@marketlogicsoftware.com>
2020-06-06 11:00:08 +0200

    address update

---
+++ src/items/veggies.js
@@ -0,0 +1,146 @@
+[
+  {
+    "itemCode": "greenchilli",
+    "brand": "",
+    "item": "fresh green chilli",
+    "unitWeight": ["100 g", "250 g", "500 g", "1 kg"],
+    "unitPrice": [1.0, 2.3, 4, 8],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/gchilli.png"
+  },
+  {
+    "itemCode": "coriander",
+    "brand": "",
+    "item": "fresh coriander",
+    "unitWeight": "1 Stuck",
+    "unitPrice": 1.2,
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/coriander.png"
+  },
+  {
+    "itemCode": "mint",
+    "brand": "",
+    "item": "fresh mint",
+    "unitWeight": "1 Stuck",
+    "unitPrice": 1.2,
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/mint.png"
+  },
+  {
+    "itemCode": "rawmango",
+    "brand": "",
+    "item": "Raw Mango",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [2, 3.5, 7],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/rawmango.png"
+  },
+  {
+    "itemCode": "okra",
+    "brand": "",
+    "item": "fresh okra / bhendi",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [2.0, 3.8, 7.5],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/okra.png"
+  },
+  {
+    "itemCode": "arbi",
+    "brand": "",
+    "item": "fresh arbi",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [2.3, 4.2, 8],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/arbi.png"
+  },
+  {
+    "itemCode": "brinjal",
+    "brand": "",
+    "item": "fresh small egg plant / small brinjal",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [2.5, 3.5, 6.5],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/brinjal.png"
+  },
+  {
+    "itemCode": "karela",
+    "brand": "",
+    "item": "fresh karela / bitter gourd",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [2.0, 3.5, 7],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/karela.png"
+  },
+  {
+    "itemCode": "drsticks",
+    "brand": "",
+    "item": "fresh drumsticks",
+    "unitWeight": ["250 g", "500 g"],
+    "unitPrice": [2.6, 5],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/drstick.png"
+  },
+  {
+    "itemCode": "chkdkya",
+    "brand": "",
+    "item": "fresh lima beans / chikudukaya",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [0, 0, 0],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/chikudukaya.png"
+  },
+  {
+    "itemCode": "btlgourd",
+    "brand": "",
+    "item": "fresh bottle gourd / louki",
+    "unitWeight": "1 kg",
+    "unitPrice": 7.5,
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/bottle_gourd.png"
+  },
+  {
+    "itemCode": "ivygourd",
+    "brand": "",
+    "item": "fresh ivy gourd / Tindly",
+    "unitWeight": ["250 g", "500 g", "1 kg"],
+    "unitPrice": [2, 3.8, 7.5],
+    "available": false,
+    "categoryCode": [
+      "FR001"
+    ],
+    "imgUrl": "/images/ivygourd.png"
+  }
+]
