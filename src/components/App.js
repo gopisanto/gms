1102b1d6d22f40832960102d1658ffd0c554b956
Santosh Kumar Gopisetty <skg@marketlogicsoftware.com>
2020-12-11 15:13:22 +0100

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,11 @@ import Col from './Col';
 import "./App.css";
 import styles from './App.style';
 
+const holiday = true;
+
+const Holiday = () => <div style={{fontSize: '22px', fontWeight: 'bold', display: 'flex', height: '100vh', overflow: 'auto', textAlign: 'center', alignItems: 'center'}}>
+  We are not processing anymore orders for now, sorry for the inconvenience. We will inform you again once we start taking orders. Thank you and Be safe.
+</div>;
 
 function App({ classes: { app, filterMenu, groceries } }) {
   const history = useHistory();
@@ -39,12 +44,21 @@ function App({ classes: { app, filterMenu, groceries } }) {
   
   return (
     <div className={app}>
-      <Header />
-      <h3>Important:- We will consider orders placed till 10th December 2020, since we will close and resume our services again from March 1st, 2021. Since we are going to India.</h3>
-      <AppBar value={filter} onChange={onFilterChange} menuClickHandler={onMenuClick} onCartClick={onCartClick} />
+      {
+        !holiday
+        &&
+        <React.Fragment>
+          <Header />
+          <h3>Important:- We will consider orders placed till 10th December 2020, since we will close and resume our services again from March 1st, 2021. Since we are going to India.</h3>
+          <AppBar value={filter} onChange={onFilterChange} menuClickHandler={onMenuClick} onCartClick={onCartClick} />
+        </React.Fragment>
+      }
       <div>
         <Switch>
-          <Route path="/" exact>
+          <Route path='/' exact>
+            <Holiday />
+          </Route>
+          <Route path="/abcdhfjhsjgfhgsfhjgshfbbewhiryhyweirhjbhfghjghjfgjhsdfgjgsdbcnbsdjfghjsdfgjsdfg" exact>
             <div className="groceryContent">
               <Col className={filterMenu}>
                 <FilterMenu itemClickHandler={onItemClick} />
@@ -64,6 +78,7 @@ function App({ classes: { app, filterMenu, groceries } }) {
             <Cart />
           </Route>
           <Route path="/address" exact>
+            <AppBar value={filter} onChange={onFilterChange} menuClickHandler={onMenuClick} onCartClick={onCartClick} />
             <Address filter={filter} />
           </Route>
         </Switch>
